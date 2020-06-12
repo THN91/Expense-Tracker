784c3794a314ea922b3985691e1406c735290670
Taras Honcharuk <taras.goncharuk22@gmail.com>
2020-06-12 14:58:10 +0300

    minus plus rendering

--- script.js
+++ script.js
@@ -30,10 +30,30 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+function updateValues() {
+    const amounts = transactions.map(transactions => transactions.amount);
+
+    const total = amounts
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+}
+
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM)
+  updateValues()
 }
 
 init();
